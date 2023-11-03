e247c4ef753e3950ac80c4192f5c88dbe15331e0
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-11-04 02:06:03 +0300

    6 transitions

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,7 +1,7 @@
 <template>
-  <Transition>
+  <Transition name="modal-outer">
     <div v-show="modalActive" class="absolute w-full bg-gray bg-opacity-30 h-screen top-0 left-0 flex justify-center px-9 ">
-     <Transition>
+     <Transition name="model-inner">
         <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md ">
             <slot />
             <button @click="$emit('close-modal')" class="text-white mt-8 bg-weather-primary py-2 px-6 "> Close </button>
@@ -24,6 +24,34 @@ defineProps({
   })
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
+
+.modal-outer-enter-active , .modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.25,0.02,0.19,1.02);
+}
+.modal-outer-enter-from , .modal-outer-leave-to {
+  opacity: 0;
+ }
+
+
+
+
+ .modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.25,0.02,0.19,1.02) 0.15s ;
+}
+  .modal-inner-leave-active {
+   transition: all 0.3s cubic-bezier(0.25,0.02,0.19,1.02)  ;
+ }
+ .modal-inner-enter-from   {
+  opacity: 0;
+  transform: scale(0.8);
+ }
+  .modal-inner-leave-to {
+ 
+  transform: scale(0.8);
+ }
+
+
+
 
 </style>
