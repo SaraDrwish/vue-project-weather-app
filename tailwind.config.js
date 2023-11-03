b82840c92ac675b6345552e087031aa88a440d91
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-11-03 19:52:41 +0300

    2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+ module.exports = {
+  content: [ "./index.html" , "./src/**/*.{vue,js,ts,jsx,tsx}" ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "weather-primary": "#ds4845",
+        "weather-secondary": "#jfdjfdkk"
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center:true
+    },
+    screens: {
+      sm: "640px",
+      md:"768px"
+    }
   },
   plugins: [],
 }
