523b348f93ba021df031d3b842b54b2c02fb14b9
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-11-05 23:18:51 +0300

    11-5-11:18

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "../views/HomeView.vue"
+import CityView from "../views/CityView.vue"
  
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,14 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
-    } ,
+    },
+    
+      {
+      path: '/weather/:state/:city',
+      name: 'city',
+      component: CityView,
+      }
+      
   ]
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 
 <template>
   <main class="container text-white">
-    <h1 class="p-4 my-9 text-center text-2xl">Home view</h1>
+    <!-- <h1 class="p-4 my-9 text-center text-2xl">Home view</h1> -->
     <div class="p-4 m-6 relative">
       <input type="text"
        v-model="searchQuery"
@@ -10,14 +10,16 @@
        class="py-2 px-1 bg-transparent w-full border-b
         focus:outline-none focus:shadow-[0px_2px_0px_0px_#004E71] focus:bg-red " >
        
-        <ul class="absolute bg-red text-white w-full shadow-md py-2 px-1 top-[68px]"
-        v-if="mapboxSearchResults"
-        >
+        <ul 
+        class="absolute bg-red text-white w-full shadow-md py- px-1 top-[66px]"
+        v-if="mapboxSearchResults">  
           <p v-if="searchError">something is goming wrong </p>
           <p v-if="!serverError && mapboxSearchResults.length === 0" >no results match your query  </p>
           <template v-else>
             <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id"
-             class="py-2 curser-pointer ">
+             class="py-2 curser-pointer "
+             @click="prevewCity(searchResult)"
+             >
              {{ searchResult.place_name }}
             </li>
           </template>
@@ -28,8 +30,26 @@
 
 <script setup>
 
+import { useRouter } from "vue-router";
 import { ref } from "vue";
-import axios from "axios"
+import axios from "axios";
+
+const router = useRouter();
+
+const prevewCity = (searchResult) => {
+  console.log(searchResult)
+  const [city, state] = searchResult.place_name.split(",");
+  console.log(city, state);
+  router.push({
+    name: "city",
+    params: { state: state.replaceAll(" " , ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true
+    },
+  });
+};
 
 const mapboxApiKey = "pk.eyJ1Ijoic2FhcmFhYWRlbGwiLCJhIjoiY2xvbHVuOGl6Mm1kNTJzcndsdGg2ODZvNSJ9.Q_EIYu5r-osuXlNy_b-HEA"
 const searchQuery = ref("");
@@ -47,8 +67,8 @@ const getSearchResults = () => {
         mapboxSearchResults.value = result.data.featuers;
         console.log(mapboxSearchResults.value)
       }
-      catch (e) {
-        console.log("eror" + e)
+      catch {
+        // console.log("eror" + e)
         searchEroor.value = true
         
       }
